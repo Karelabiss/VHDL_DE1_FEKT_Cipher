08e864904aa7f711cfcb35625952dae1059fda2e
domo220 <115161882+domo220@users.noreply.github.com>
2024-04-23 18:55:15 +0200

    Update Vernam_cipher_tb.vhd

--- Vernam_cipher/Vernam_cipher_tb.vhd
+++ Vernam_cipher/Vernam_cipher_tb.vhd
@@ -1,44 +1,47 @@
 -- Testbench automatically generated online
 -- at https://vhdl.lapinoo.net
--- Generation date : 9.4.2024 19:10:45 UTC
+-- Generation date : 23.4.2024 12:28:15 UTC
 
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity tb_random_generator is
-end tb_random_generator;
+entity tb_Vernam_cipher is
+end tb_Vernam_cipher;
 
-architecture tb of tb_random_generator is
+architecture tb of tb_Vernam_cipher is
 
-    component random_generator
-    port (clk                : in std_logic;
-        reset              : in std_logic;
-        txt                : in std_logic_vector (7 downto 0);
-        code_txt           : out std_logic_vector (7 downto 0);
-        decode_txt         : out std_logic_vector (7 downto 0);
-        array_of_key_debug : out std_logic_vector (7 downto 0));
+    component Vernam_cipher
+    port (clk             : in std_logic;
+        input           : in std_logic_vector (7 downto 0);
+        shift           : in std_logic_vector (7 downto 0);
+        coded_txt_input : in std_logic_vector (7 downto 0);
+        SW              : in std_logic;
+        decode_output   : out std_logic_vector (7 downto 0);
+        code_output     : out std_logic_vector (7 downto 0));
 end component;
 
-signal clk                : std_logic;
-signal reset              : std_logic;
-signal txt                : std_logic_vector (7 downto 0);
-signal code_txt           : std_logic_vector (7 downto 0);
-signal decode_txt         : std_logic_vector (7 downto 0);
-signal array_of_key_debug : std_logic_vector (7 downto 0);
+signal clk             : std_logic;
+signal input           : std_logic_vector (7 downto 0);
+signal shift           : std_logic_vector (7 downto 0);
+signal coded_txt_input : std_logic_vector (7 downto 0);
+signal SW              : std_logic;
+signal decode_output   : std_logic_vector (7 downto 0);
+signal code_output     : std_logic_vector (7 downto 0);
 
-constant TbPeriod : time := 100 ns; -- EDIT Put right period here
+constant TbPeriod : time := 1000 ns; -- EDIT Put right period here
 signal TbClock : std_logic := '0';
 signal TbSimEnded : std_logic := '0';
 
 begin
 
-    dut : random_generator
-    port map (clk                => clk,
-        reset              => reset,
-        txt                => txt,
-        code_txt           => code_txt,
-        decode_txt         => decode_txt,
-        array_of_key_debug => array_of_key_debug);
+    dut : Vernam_cipher
+    port map (clk             => clk,
+        input           => input,
+        shift           => shift,
+        coded_txt_input => coded_txt_input,
+        SW              => SW,
+        decode_output   => decode_output,
+        code_output     => code_output);
 
     -- Clock generation
     TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
@@ -46,28 +49,19 @@ begin
     -- EDIT: Check that clk is really your main clock signal
     clk <= TbClock;
 
-    
-    
-    
     stimuli : process
     begin
-        
-        -- Reset generation
-        -- EDIT: Check that reset is really your reset signal
-        reset <= '1';
-        wait for 10 ns;
-        reset <= '0';
-        wait for 10 ns;
-        
-        
         -- EDIT Adapt initialization as needed
-        txt <= "01000001";
+        SW <= '0';
         wait for 100 ns;
-        txt <= "01001000";
+        input <= b"0100_0001";
         wait for 100 ns;
-        txt <= "01001111";
+        shift <= b"0000_0011";
         wait for 100 ns;
-        txt <= "01001010";
+        SW <= '1';
+        wait for 100 ns;
+        coded_txt_input <=b"0100_0010";
+        
 
         -- EDIT Add stimuli here
         wait for 100 * TbPeriod;
@@ -81,7 +75,7 @@ end tb;
 
 -- Configuration block below is required by some simulators. Usually no need to edit.
 
-configuration cfg_tb_random_generator of tb_random_generator is
+configuration cfg_tb_Vernam_cipher of tb_Vernam_cipher is
     for tb
 end for;
-end cfg_tb_random_generator;
+end cfg_tb_Vernam_cipher;
