ac56f785002db751fedc5ae128791482bce5d04b
domo220 <115161882+domo220@users.noreply.github.com>
2024-04-23 17:45:12 +0200

    Update Ceaser_cipher_tb.vhd

--- Ceaser_cipher/Ceaser_cipher_tb.vhd
+++ Ceaser_cipher/Ceaser_cipher_tb.vhd
@@ -15,21 +15,21 @@ architecture tb of tb_BinaryAdder is
     port (clk             : in std_logic;
         input           : in std_logic_vector (7 downto 0);
         shift           : in std_logic_vector (7 downto 0);
-        decode_input    : out std_logic_vector (7 downto 0);
-        SW              : in std_logic_vector (1 downto 0);
+        decode_output    : out std_logic_vector (7 downto 0);
+        SW              : in std_logic;
         coded_txt_input : in std_logic_vector (7 downto 0);
-        code_input      : out std_logic_vector (7 downto 0));
+        code_output      : out std_logic_vector (7 downto 0));
 end component;
 
 signal clk             : std_logic;
 signal input           : std_logic_vector (7 downto 0);
 signal shift           : std_logic_vector (7 downto 0);
-signal decode_input    : std_logic_vector (7 downto 0);
-signal SW              : std_logic_vector (1 downto 0);
+signal decode_output    : std_logic_vector (7 downto 0);
+signal SW              : std_logic;
 signal coded_txt_input : std_logic_vector (7 downto 0);
-signal code_input      : std_logic_vector (7 downto 0);
+signal code_output      : std_logic_vector (7 downto 0);
 
-constant TbPeriod : time := 100 ns; -- EDIT Put right period here
+constant TbPeriod : time := 10 ns; -- EDIT Put right period here
 signal TbClock : std_logic := '0';
 signal TbSimEnded : std_logic := '0';
 
@@ -39,10 +39,10 @@ begin
     port map (clk             => clk,
         input           => input,
         shift           => shift,
-        decode_input    => decode_input,
+        decode_output    => decode_output,
         SW              => SW,
         coded_txt_input => coded_txt_input,
-        code_input      => code_input);
+        code_output      => code_output);
 
     -- Clock generation
     TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
@@ -53,34 +53,28 @@ begin
     stimuli : process
     begin
                -- Test cases
-        SW(1)<= '0';
-        SW(0)<= '1';
+        shift <= "00000010";
+        SW<= '0';
+        wait for 10 ns;
         input <= "01000001";
-        shift <= "00000010";
-        wait for 100 ns;
-        
+        wait for 150 ns;    
         input <= "01001000";
-        shift <= "00000010";
-        wait for 100 ns;
-        
+        wait for 80 ns;
         input <= "01001111";
-        shift <= "00000010";
-        wait for 100 ns;
-        
+        wait for 30 ns;
         input <= "01001010";
-        shift <= "00000010";
-        wait for 100 ns;
+        wait for 15 ns;
+
         
-        SW(1)<= '1';
-        SW(0)<= '0';
+        SW<= '1';
         coded_txt_input <= "01000011";
-        wait for 100 ns;
+        wait for 10 ns;
         coded_txt_input <= "01001010";
-        wait for 100 ns;
+        wait for 10 ns;
         coded_txt_input <= "01010001";
-        wait for 100 ns;
+        wait for 10 ns;
         coded_txt_input <= "01001100";
-        wait for 100 ns;
+        wait for 10 ns;
         
 
         -- EDIT Add stimuli here
@@ -99,3 +93,6 @@ configuration cfg_tb_BinaryAdder of tb_BinaryAdder is
     for tb
 end for;
 end cfg_tb_BinaryAdder;
+
+
+
